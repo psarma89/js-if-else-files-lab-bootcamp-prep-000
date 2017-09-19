b15cca80c4ccd599bc77abdce2ee559c9f078fb4
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 17:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 var name = "Susan"
-
-if (name==="Susan"){
-  console.log("This name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
-
-var height = "74"
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected the height to be 70")
-}
-
+var height = "70"
 // Don't worry about this
 module.exports = { name, height
 }
